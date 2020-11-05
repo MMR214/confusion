cf563466429913e0dc90fb3abffb6f62a05524d7
Mick Ogeir <mickriego214@gmail.com+>
2020-11-05 22:50:57 +0400

    My 2nd Commit

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
+import Menu from './components/MenuComponents';
+
+
+class Dish extends Component {
+
+
+}
+
+export default Dish;
